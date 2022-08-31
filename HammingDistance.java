97eac778cba272704625389a56cd34ca803f1b61
Gowreesh A.M <manekshaw70@gmail.com>
2022-08-31 10:04:13 +0530

    leetcode 461

---
+++ HammingDistance.java
@@ -0,0 +1,12 @@
+class HammingDistance {
+    public int hammingDistance(int x, int y) {
+        int xor=x^y;
+        int i=0,d=0;
+        while(xor>>i!=0){
+            if(((xor>>i)&1)==1)
+                d++;
+            i++;
+        }
+        return d;
+    }
+}
