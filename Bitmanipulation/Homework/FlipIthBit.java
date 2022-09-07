49effb473c4f74e13d8ce1c13f53e74397339082
Gowreesh A.M <manekshaw70@gmail.com>
2022-09-07 10:25:08 +0530

    file upload

--- Bitmanipulation/Homework/FlipIthBit.java
+++ Bitmanipulation/Homework/FlipIthBit.java
@@ -1,6 +1,6 @@
 package Bitmanipulation.Homework;
 
-public class FlipIthBit {
+ class FlipIthBit {
     public static void main(String[] args) {
         int a=12,pos=3;
         System.out.println(a^(1<<pos-1));
