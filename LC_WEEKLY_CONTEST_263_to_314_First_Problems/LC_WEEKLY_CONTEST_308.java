e92b5805622d7e83ad72a2be5f33e226a2f22038
Gowreesh A.M <manekshaw70@gmail.com>
2022-10-11 10:49:41 +0530

    file upload

---
+++ LC_WEEKLY_CONTEST_263_to_314_First_Problems/LC_WEEKLY_CONTEST_308.java
@@ -0,0 +1,21 @@
+package LC_WEEKLY_CONTEST_263_to_314_First_Problems;
+import java.util.*;
+public class LC_WEEKLY_CONTEST_308 {
+    public int[] answerQueries(int[] nums, int[] queries) {
+        Arrays.sort(nums);
+        int[]res=new int[queries.length];
+        for(int i=0;i<queries.length;i++){
+            int sum=0,j=0;
+            for(j=0;j<nums.length;j++){
+                if(sum+nums[j]>queries[i]){
+                    res[i]=j;
+                    break;
+                }
+                sum+=nums[j];
+            }
+            if(j==nums.length)
+                res[i]=j;
+        }
+        return res;
+    }
+}
