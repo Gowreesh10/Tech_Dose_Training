40ad65db2696760d1a167c014e10b26f5b5823f5
Gowreesh A.M <manekshaw70@gmail.com>
2022-10-18 11:02:55 +0530

    file upload

---
+++ LC_CONTEST_First50_Problems/LC_WEEKLY_CONTEST_264.java
@@ -0,0 +1,26 @@
+package LC_CONTEST_First50_Problems;
+
+public class LC_WEEKLY_CONTEST_264 {
+    static boolean isValid(String str){
+        int hypen=0;
+        for(int i=0;i<str.length();i++){
+            if(Character.isDigit(str.charAt(i))) return false;
+            if(Character.isLowerCase(str.charAt(i))) continue;
+            if(str.charAt(i)=='-'){
+                if(++hypen>1) return false;
+                if(i-1<0||!Character.isLowerCase(str.charAt(i-1))||
+                        i+1>=str.length()||!Character.isLowerCase(str.charAt(i+1))) return false;
+            }else if(i!=str.length()-1) return false;
+        }
+        return true;
+    }
+    public int countValidWords(String sentence) {
+        if(sentence.length()==0) return 0;
+        String[] str=sentence.trim().split("\\s+");
+        int count=0;
+        for(String i:str){
+            if(isValid(i)) count++;
+        }
+        return count;
+    }
+}
