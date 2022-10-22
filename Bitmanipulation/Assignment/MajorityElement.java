1d9a8a24d90494dcb69f139366dcc64741ae9c1d
Gowreesh A.M <manekshaw70@gmail.com>
2022-10-22 16:43:21 +0530

    file upload

--- Bitmanipulation/Assignment/MajorityElement.java
+++ Bitmanipulation/Assignment/MajorityElement.java
@@ -1,8 +1,20 @@
 package Bitmanipulation.Assignment;
-import java.util.*;
 public class MajorityElement {
-        public int majorityElement(int[] nums) {
-            Arrays.sort(nums);
-            return(nums[nums.length/2]);
+    public int majorityElement(int[] nums) {
+//        Arrays.sort(nums);
+//        return(nums[nums.length/2]);  O(nlongn)
+
+        //boyer Moore's voting Algorithm for element appearing >n/2 times
+        int major=nums[0],count=1;
+        for(int i=1;i<nums.length;i++){
+            if(count==0){
+                major=nums[i];
+                count=1;
+            }
+            else if(major==nums[i]) count++;
+            else count--;
         }
+        return major;
+        //TC : O(N) SC : O(1)
+    }
 }
