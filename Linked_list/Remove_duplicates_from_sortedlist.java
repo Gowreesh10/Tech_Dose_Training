3719cb3576c2298a8af6e8189ae9d27e3c2ff168
Gowreesh A.M <manekshaw70@gmail.com>
2022-10-31 17:26:53 +0530

    file upload

---
+++ Linked_list/Remove_duplicates_from_sortedlist.java
@@ -0,0 +1,35 @@
+package Linked_list;
+
+public class Remove_duplicates_from_sortedlist {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head,fast = head;
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow = reverse(slow);
+        while(slow != null)
+        {
+            if(head.val != slow.val)
+            {
+                return false;
+            }
+            head = head.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+    public static ListNode reverse(ListNode c)
+    {
+        ListNode next = c,prev = null;
+        while(c != null)
+        {
+            next = c.next;
+            c.next = prev;
+            prev = c;
+            c = next;
+        }
+        return prev;
+    }
+}
---
+++ Search_Algorithm/BinarySearch/SearchElementsInRotatedSortedArray.java
@@ -0,0 +1,5 @@
+package Search_Algorithm.BinarySearch;
+
+public class SearchElementsInRotatedSortedArray {
+
+}
